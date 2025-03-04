ded0e7c6422715a3eabc358e461badd9c71f8316
Nurbakyt <n79907561@gmail.com>
2025-03-04 12:38:23 +0600

    add supa

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
+import { supabase } from './supabase'
+
 
 const App = () => {
+
+  const getData = async () => {
+    let { data: category, error } = await supabase
+      .from('category')
+      .select('*');
+
+
+    let { data: reviews,  } = await supabase
+      .from('reviews')
+      .select('*')
+
+
+    console.log(category);
+    console.log(reviews);
+
+
+  }
+
+  React.useEffect(() => {
+    getData()
+
+  }, [])
+
   return (
     <div>
       fffff
